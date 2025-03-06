15177391ef47b06505534155b000ffc9dcd9f92d
SergioKosh <ksa27101990@gmail.com>
2025-03-06 23:33:12 +0000

    Patient class

---
+++ HospitalApp/Patient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalApp
+{
+    internal class Patient
+    {
+        public string Name { get; set; }
+        public string BloodType { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public int YearsOld { get; set; }
+
+        //constructor 
+        public Patient(string name, string bloodType, DateTime birthday)
+        {
+            DateTime today = DateTime.Today;
+
+            Name = name;
+            BloodType = bloodType;
+            Birthday = birthday;
+            YearsOld = today.Year - birthday.Year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({YearsOld}) Type:{BloodType}";
+        }
+
+    }
+
+
+
+}
---
+++ HospitalApp/Ward.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalApp
+{
+    internal class Ward
+    {
+        public string WardName { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Patient> Patients { get; set; } = new List<Patient>();
+
+        public override string ToString()
+        {
+            return $"{WardName} (Limit: {Capacity - Patients.Count})";
+        }
+    }
+}

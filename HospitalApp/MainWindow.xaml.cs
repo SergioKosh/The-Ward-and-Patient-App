9a876229edae495618291a29921a7c8f702bf46f
SergioKosh <ksa27101990@gmail.com>
2025-03-08 21:47:47 +0000

    listboxes

--- HospitalApp/MainWindow.xaml.cs
+++ HospitalApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,8 +23,9 @@ namespace HospitalApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<Ward> Wards { get; set; } = new ObservableCollection<Ward>();
-        ObservableCollection<HospitalApp.Patient> patients = new ObservableCollection<Patient> ();
+        private bool isLoaded = false;
+        public ObservableCollection<HospitalApp.Ward> Wards { get; set; } = new ObservableCollection<Ward>();
+        public ObservableCollection<HospitalApp.Patient> patients = new ObservableCollection<Patient>();
         public MainWindow()
         {
             InitializeComponent();
@@ -32,15 +35,46 @@ namespace HospitalApp
         //startup code
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Ward w1 = new HospitalApp.Ward("w1",10);
-            Ward w2 = new HospitalApp.Ward("w2", 7);
-            Ward w3 = new HospitalApp.Ward("w3", 5);
+            
+            
+            isLoaded = true;
+      
+        }
 
-            Wards.Add(w1);
-            Wards.Add(w2);
-            Wards.Add(w3);
+        private void lbxWardList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbxWardList.SelectedItem is Ward selectedWard)
+            {
+                lbxPatientList.ItemsSource = selectedWard.Patients;
+            }
+            else
+            {
+                lbxPatientList.ItemsSource = null; // Clear list if no ward is selected
+            }
+        }
 
-            lbxWardList.ItemsSource = Wards;
+        //slider, but stil not working
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (!isLoaded) return;
+
+
+            tbxSliderValue.Text = string.Format("{0:F0}", sliderWard.Value);
+
+        }
+
+        //checking which radio buttom selected 
+        private string GetSelectedBloodType()
+        {
+            if (rbA.IsChecked == true) return "A";
+            if (rbB.IsChecked == true) return "B";
+            if (rbAB.IsChecked == true) return "AB";
+            if (rbO.IsChecked == true) return "O";
+
+            MessageBox.Show("Please select a blood type!", "Selection Error", MessageBoxButton.OK, 
+            MessageBoxImage.Warning);
+            
+            return string.Empty;
         }
 
         private void btnAddWard_Click(object sender, RoutedEventArgs e)
@@ -48,21 +82,51 @@ namespace HospitalApp
             //check if the fields are empty
             if (string.IsNullOrWhiteSpace(tbxNameWard.Text) || sliderWard.Value == 0)
             {
-                
+
                 MessageBox.Show("Error: The input cannot be empty!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
+
+            string wardName = tbxNameWard.Text;
+            int capacity = Convert.ToInt16(sliderWard.Value);
+            Wards.Add(new HospitalApp.Ward(wardName, capacity));
+            MessageBox.Show("The ward was added successfully!", "Success", MessageBoxButton.OK);
             
-                string wardName = tbxNameWard.Text;
-                int capacity = Convert.ToInt16(sliderWard.Value);
-                Wards.Add(new HospitalApp.Ward(wardName, capacity));
-                MessageBox.Show("The ward was added successfully!", "Success", MessageBoxButton.OK);
-            
+            //refresh listbox
+            lbxWardList.ItemsSource = null;
+            lbxWardList.ItemsSource = Wards;
+
         }
 
-        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        
+
+        private void btnAddPatient_Click(object sender, RoutedEventArgs e)
         {
-            tbxSliderValue.Text = string.Format("{0:F0}", sliderWard.Value);
+            Ward selectedWard = lbxWardList.SelectedItem as Ward;
+
+            if (selectedWard == null)
+            {
+                MessageBox.Show("Ward is not selected");
+                return;
+            }
+            else if (selectedWard.Patients.Count == selectedWard.Capacity)
+            {
+                MessageBox.Show("The ward is full, select another one");
+                return;
+            }
+
+            string bloodType = GetSelectedBloodType();
+            string name = tbxNamePatient.Text;
+            DateTime birthday = Convert.ToDateTime(dpDOB.Text);
+                
+            Patient pat = new Patient(name, bloodType, birthday);
+            selectedWard.Patients.Add(pat);
+            MessageBox.Show($"Patient {name} has been added to {selectedWard.WardName}.", "Success", MessageBoxButton.OK);
+            
+            //refresh Patient listbox
+            lbxPatientList.ItemsSource = null;
+            lbxPatientList.ItemsSource = selectedWard.Patients;
+
         }
     }
 }
